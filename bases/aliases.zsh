49da3099c13c431d761c1f0e584fe2603af7e08a
line <line@live.com>
2023-01-02 01:53:18 +0800

    feat: 添加别名

---
+++ bases/aliases.zsh
@@ -0,0 +1,14 @@
+alias e="nvim"
+alias ls="exa"
+alias cat="bat --style=numbers,changes,header"
+alias l="ls -hl --no-user --time-style iso"
+alias la="ls -a"
+alias rm="rm -i"
+alias mv="mv -i"
+alias cp="cp -i"
+alias md="mkdir"
+alias rd="rmdir"
+alias ...='../..'
+alias ....='../../..'
+alias .....='../../../..'
+alias ......='../../../../..'
