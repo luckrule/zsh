6042827b94150c74a046c627c0503591fc2a1dad
line <line@live.com>
2023-01-02 01:33:43 +0800

    feat: 添加按键绑定

---
+++ bases/keybinds.zsh
@@ -0,0 +1,12 @@
+zle -N zle-line-init
+zle -N zle-line-finish
+zle -N up-line-or-beginning-search
+zle -N down-line-or-beginning-search
+
+bindkey -e
+bindkey -M emacs "${terminfo[kpp]}" up-line-or-history
+bindkey -M emacs "${terminfo[knp]}" down-line-or-history
+bindkey -M emacs "${terminfo[kcuu1]}" up-line-or-beginning-search
+bindkey -M emacs "${terminfo[kcud1]}" down-line-or-beginning-search
+bindkey -M emacs "${terminfo[khome]}" beginning-of-line
+bindkey -M emacs "${terminfo[kend]}" end-of-line
