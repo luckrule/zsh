d1d66b50e72785d689fdfd1927cf437caf6f34d5
line <line@live.com>
2023-01-03 14:38:04 +0800

    fix(aliases.zsh): 修复别名
    l

--- bases/aliases.zsh
+++ bases/aliases.zsh
@@ -1,7 +1,7 @@
 alias e="nvim"
 alias ls="exa"
 alias cat="bat --style=numbers,changes,header"
-alias l="ls -hl --no-user --changed --time-style iso"
+alias l="ls -hl --changed --time-style iso"
 alias la="ls -a"
 alias ll="l -a"
 alias lt="l -T --git-ignore"
